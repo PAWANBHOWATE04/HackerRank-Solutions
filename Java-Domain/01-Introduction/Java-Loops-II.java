f1787b408b07a7765103a92e32a073e01f5ed169
PAWANBHOWATE04 <pawanbhowate04@gmail.com>
2026-01-10 19:56:17 +0530

    Solved: Java Loops II

--- Java-Domain/01-Introduction/Java-Loops-II.java
+++ Java-Domain/01-Introduction/Java-Loops-II.java
@@ -1,1 +1,28 @@
+import java.io.*;
+import java.util.*;
 
+class Solution {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int sum = a;
+            for (int j = 0; j < n; j++) {
+                // Using bitwise left shift (1 << j) is 
+                // more efficient for calculating 2^j
+                sum += (1 << j) * b;
+
+                // print() keeps the series on a single line
+                System.out.print(sum + " ");
+            }
+            // println() moves to a new line for the next query result
+            System.out.println();
+        }
+        in.close();
+    }
+}
